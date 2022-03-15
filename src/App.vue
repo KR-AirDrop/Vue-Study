03683872dd87362d561f48075ff953aeb8b69d1f
KR-AirDrop <choish4682@naver.com>
2022-03-15 22:57:17 +0900

    mounted....

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <!-- v-if 는 조건이 참일 때만 HTML 보여줌 -->
-  <ProductDetail
-    @closeModal="모달창열렸니 = false"
-    :onerooms="onerooms"
-    :clickroom="clickroom"
-    :모달창열렸니="모달창열렸니"
-  />
+  <transition name="fade">
+    <ProductDetail
+      @closeModal="모달창열렸니 = false"
+      :onerooms="onerooms"
+      :clickroom="clickroom"
+      :모달창열렸니="모달창열렸니"
+    />
+  </transition>
 
   <div class="menu">
     <a v-for="(a, i) in menu" :key="i">{{ a }}</a>
   </div>
 
-  <DiscountModal />
+  <transition name="fade">
+    <DiscountModal v-if="showDiscount == true" />
+  </transition>
+
+  <button @click="priceSort">낮은가격순</button>
+  <button @click="priceSortReverse">높은가격순</button>
+  <button @click="titleSort">이름순</button>
+  <button @click="sortReturn">원래대로</button>
 
   <CardList
     @openModal="
@@ -39,17 +48,53 @@ export default {
       menu: ["Home", "Products", "About"],
       products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
       report: [0, 0, 0],
-      onerooms: data,
+      originalOnerooms: data,
+      onerooms: [...data],
       clickroom: 0,
       모달창열렸니: false,
+      showDiscount: true,
     };
   },
   methods: {
     // 함수 보관함
-    increase(i) {
-      this.report[i]++;
+    priceSort() {
+      this.onerooms.sort(function (a, b) {
+        return a.price - b.price;
+      });
+    },
+    priceSortReverse() {
+      this.onerooms.sort(function (a, b) {
+        return b.price - a.price;
+      });
+    },
+    titleSort() {
+      this.onerooms.sort(function (a, b) {
+        let x = a.title.toLowerCase();
+        let y = b.title.toLowerCase();
+        if (x < y) {
+          return -1;
+        }
+        if (x > y) {
+          return 1;
+        }
+        return 0;
+      });
+    },
+    sortReturn() {
+      this.onerooms = [...this.originalOnerooms];
     },
   },
+
+  //데이터만 생성
+  created() {},
+
+  // 마운트 이후
+  mounted() {
+    setTimeout(() => {
+      this.showDiscount = false;
+    }, 3000);
+  },
+
   components: {
     DiscountModal,
     ProductDetail,
@@ -113,5 +158,24 @@ div {
   border: none;
   font-weight: bold;
   cursor: pointer;
+}
+
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: all 1s;
+}
+.fade-enter-to {
+  opacity: 1;
+}
+.fade-leave-from {
+  opacity: 1;
+}
+.fade-leave-active {
+  transition: all 1s;
+}
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
