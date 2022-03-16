6c3fcc177e8eb52275b9f760c29f5e238a601957
KR-AirDrop <choish4682@naver.com>
2022-03-16 14:44:20 +0900

    props 공부중

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Post :vuestaData="vuestaData" />
+    <Post />
+    <Post />
+  </div>
+</template>
+
+<script>
+import Post from "./Post.vue";
+export default {
+  name: "Container",
+  components: {
+    Post,
+  },
+  props: {
+    vuestaData: Array,
+  },
+};
+</script>
+
+<style></style>
